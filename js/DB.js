47783e39e60289d8534eecfb8dd04763f6ce4848
sam-mcginnis <sammcg96@aol.com>
2022-09-11 10:48:40 -0400

    final mvp

--- js/DB.js
+++ js/DB.js
@@ -4,6 +4,7 @@ function storeData() {
     localStorage.setItem("rejectedDescriptionsFile", JSON.stringify(rejectedDescriptionsFile))
     localStorage.setItem("spendingCategories", JSON.stringify(spendingCategories))
     localStorage.setItem("event_data", JSON.stringify(event_data))
+    localStorage.setItem("todoList", JSON.stringify(todoList))
 }
 
 function loadData(){
@@ -11,6 +12,7 @@ function loadData(){
     let setRejectedDescriptionsFile = JSON.parse(localStorage.getItem("rejectedDescriptionsFile"))
     let setSpendingCategories = JSON.parse(localStorage.getItem("spendingCategories"))
     let setEvent_data = JSON.parse(localStorage.getItem("event_data"))
+    let setTodoList = JSON.parse(localStorage.getItem("todoList"))
 
     if(setRejectedDescriptionsFile != null){
         rejectedDescriptionsFile = setRejectedDescriptionsFile
@@ -23,6 +25,10 @@ function loadData(){
     }
     if(setEvent_data != null){
         event_data = setEvent_data
+    }
+    if(setTodoList != null){
+        todoList = setTodoList
+        printListOnLoad()
     }
     if(statementsByYear.length != 0){
         pushDataToCharts(statementsByYear[0][1])
--- js/todoList.js
+++ js/todoList.js
@@ -1,3 +1,5 @@
+var todoList = [];
+
 (function($) {
     'use strict';
     $(function() {
@@ -5,10 +7,12 @@
       var todoListInput = $('.todo-list-input');
       $('.todo-list-add-btn').on("click", function(event) {
         event.preventDefault();
-  
+        $(this).trigger("blur");
         var item = $(this).prevAll('.todo-list-input').val();
-  
         if (item) {
+          let wrapper = []
+          wrapper.push(item.trim())
+          todoList.push(wrapper)
           todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + item + "<i class='input-helper'></i></label></div><i class='remove fa fa-trash-o'></i></li>");
           todoListInput.val("");
         }
@@ -16,9 +20,23 @@
       });
   
       todoListItem.on('change', '.checkbox', function() {
+        let todoItem = $(this).parent()[0].innerText
+
         if ($(this).attr('checked')) {
           $(this).removeAttr('checked');
+          for(let i = 0; i < todoList.length; i++){
+            if(todoList[i][0] == todoItem){
+              todoList[i].pop()
+              break
+            }
+          }
         } else {
+          for(let i = 0; i < todoList.length; i++){
+            if(todoList[i][0] == todoItem){
+              todoList[i].push("check")
+              break
+            }
+          }
           $(this).attr('checked', 'checked');
         }
   
@@ -27,8 +45,29 @@
       });
   
       todoListItem.on('click', '.remove', function() {
+        
+        let todoItem = $(this).parent()[0].innerText
+        for(let i = 0; todoList.length; i++){          
+          if(todoList[i][0] == todoItem){
+            todoList.splice(i, 1)
+            break;
+          }
+        }
         $(this).parent().remove();
       });
-  
+
+      
+
     });
   })(jQuery);
+
+  function printListOnLoad(){
+    for(let i = 0; i < todoList.length; i++){
+      if(todoList[i][1] == "check"){
+        jQuery('.todo-list').append("<li class='completed'><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' checked='checked' />" + todoList[i][0] + "<i class='input-helper'></i></label></div><i class='remove fa fa-trash-o'></i></li>");
+      }
+      else{
+        jQuery('.todo-list').append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + todoList[i][0] + "<i class='input-helper'></i></label></div><i class='remove fa fa-trash-o'></i></li>");
+      }
+    }
+  }
